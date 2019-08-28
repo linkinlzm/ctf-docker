f35a1cd84664304b17494c81797393d45be98efa
hustcw <hustcw1998@gmail.com>
2019-08-28 18:28:10 +0800

    add pip proxy

--- ubuntu1804.sh
+++ ubuntu1804.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker build -t zzh1996/ctf_ubuntu_1804 - <<DOCKERFILE_EOF || exit 1
+docker build -t hustcw/ctf_ubuntu_1804 - <<DOCKERFILE_EOF || exit 1
 from ubuntu:18.04
 run rm /etc/dpkg/dpkg.cfg.d/excludes
 run sed -i 's/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list \
@@ -27,16 +27,17 @@ run DEBIAN_FRONTEND=noninteractive \
 run useradd -ms /usr/bin/zsh ctf && \
     adduser ctf sudo && \
     echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
 user ctf
 workdir /home/ctf
 run sudo chown -R ctf:ctf /usr/local
 env PATH="/home/ctf/.local/bin:\${PATH}"
 
-run pip3 install --user -U pip && \
-    pip2 install --user -U pip
-run pip3 install --user -U ipython pycrypto pycryptodomex gmpy2 gmpy sympy numpy virtualenv requests flask angr formatstring mtp  && \
-    pip3 install --user -U git+https://github.com/arthaud/python3-pwntools.git && \
-    pip2 install --user -U ipython pycrypto pycryptodomex gmpy2 gmpy sympy numpy virtualenv requests flask angr pwntools ropgadget 
+run pip3 install --user -i https://pypi.tuna.tsinghua.edu.cn/simple -U pip && \
+    pip2 install --user -i https://pypi.tuna.tsinghua.edu.cn/simple -U pip
+run pip3 install --user -i https://pypi.tuna.tsinghua.edu.cn/simple -U ipython pycrypto pycryptodomex gmpy2 gmpy sympy numpy virtualenv requests flask angr formatstring mtp  && \
+    pip3 install --user -i https://pypi.tuna.tsinghua.edu.cn/simple -U git+https://github.com/arthaud/python3-pwntools.git && \
+    pip2 install --user -i https://pypi.tuna.tsinghua.edu.cn/simple -U ipython pycrypto pycryptodomex gmpy2 gmpy sympy numpy virtualenv requests flask angr pwntools ropgadget 
 
 run wget https://bitbucket.org/pypy/pypy/downloads/pypy2.7-v7.0.0-linux64.tar.bz2 -P /tmp/ && \
     tar xf /tmp/pypy2.7-v7.0.0-linux64.tar.bz2 && \
@@ -65,7 +66,7 @@ run sh -c "\$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/to
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && \
     git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions && \
     git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && \
-    curl https://raw.githubusercontent.com/wklken/vim-for-server/master/vimrc > ~/.vimrc \
+    curl https://raw.githubusercontent.com/wklken/vim-for-server/master/vimrc > ~/.vimrc 
     
 run mkdir mount
 workdir /home/ctf/mount
@@ -79,4 +80,4 @@ docker run -it --rm --privileged --cap-add=SYS_PTRACE \
     --hostname ctf_docker \
     --name ctf_ubuntu_1804 \
     -e TZ=Asia/Shanghai \
-    zzh1996/ctf_ubuntu_1804
+    hustcw/ctf_ubuntu_1804
