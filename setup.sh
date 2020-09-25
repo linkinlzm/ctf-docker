3987d8561fb3dfbb3d140bf187737c304fce718a
linkinlzm <11963183+linkinlzm@users.noreply.github.com>
2020-09-25 21:41:16 +0800

    add sh script

---
+++ setup.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+echo "[*] Setup Start\n\n"
+
+sudo sed -i 's/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list
+sudo sed -i 's/# deb-src/deb-src/g' /etc/apt/sources.list
+echo "[*] apt source changed"
+
+sudo apt install -y locales && sudo apt clean
+sudo sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sudo dpkg-reconfigure --frontend=noninteractive locales
+sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
+echo "[*] locales updated"
+
+sudo apt install -y git sudo bash make nano vim zsh tmux cmake binutils nasm gcc gdb g++ gcc-multilib \
+    build-essential libc6-dev-i386 libc6-dbg libc6-dbg:i386 \
+    python python-pip python3 python3-pip curl netcat htop iotop iftop man strace ltrace wget \
+    manpages-posix manpages-posix-dev libgmp3-dev libmpfr-dev libmpc-dev \
+    nmap zmap libssl-dev inetutils-ping dnsutils whois mtr net-tools iproute2 tzdata ruby \
+    ssh
+sudo apt-get source libc6-dev
+sudo apt clean
+echo "[*] tools installed"
+
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple --user --no-cache-dir -U pip
+pip3 install --upgrade pip
+pip2 install -i https://pypi.tuna.tsinghua.edu.cn/simple --user --no-cache-dir -U pip
+pip2 install --upgrade pip
+echo "[*] pip updated"
+
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple --user --no-cache-dir -U jupyterlab ipython pycrypto pycryptodomex gmpy2 gmpy sympy numpy virtualenv requests flask formatstring mtp capstone --use-feature=2020-resolver
+pip3 install --user --no-cache-dir -U git+https://github.com/arthaud/python3-pwntools.git --use-feature=2020-resolver
+pip2 install -i https://pypi.tuna.tsinghua.edu.cn/simple --user --no-cache-dir -U jupyterlab ipython pycrypto pycryptodomex gmpy2 gmpy sympy numpy virtualenv requests flask pwntools ropgadget capstone --use-feature=2020-resolver
+echo "[*] python tools installed"
+
+cd ~
+curl -LROJ https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+sudo bash install-release.sh
+read -p "Enter domain: \n" domain
+read -p "Enter UUID: \n" uuid
+read -p "Enter path: \n" path
+sudo cat<<EOF>/usr/local/etc/v2ray/config.json
+{
+  "log" : {
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "listen" : "127.0.0.1",
+      "protocol" : "http",
+      "tag" : "httpinbound",
+      "port" : 7890
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "vmess",
+      "settings": {
+        "vnext": [
+          {
+            "address": "$domain",
+            "port": 443,
+            "users": [
+                {"id": "$uuid", "alterId" : 64, "level" : 1}
+            ]
+          }
+        ]
+      },
+    "tag" : "HI",
+    "streamSettings" : {
+      "tlsSettings" : {
+          "allowInsecure" : false,
+          "alpn" : [
+            "http/1.1"
+          ],
+          "serverName" : "$domain",
+          "allowInsecureCiphers" : true
+        },
+      "wsSettings" : {
+        "path" : "$path"
+      },
+      "security" : "tls",
+      "network" : "ws"
+    }
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "block"
+    },
+    {
+      "protocol": "freedom",
+      "tag": "direct",
+      "settings": {}
+    }
+  ],
+  "routing": {
+   "domainStrategy": "IPOnDemand",
+    "strategy": "rules",
+    "rules": [
+      {
+        "type": "field",
+        "ip": [
+          "geoip:private"
+        ],
+        "outboundTag": "direct"
+      },
+      {
+        "type": "field",
+        "domain": [
+          "geosite:cn"
+        ],
+        "outboundTag": "direct"
+      },
+      {
+        "type": "field",
+        "ip": [
+          "geoip:cn"
+        ],
+        "outboundTag": "direct"
+      }
+    ]
+  }
+}
+EOF
+
+sudo systemctl enable v2ray
+sudo systemctl start v2ray
+echo "[*] v2ray installed"
+
+
+
+
+
--- zshrc
+++ zshrc
@@ -1,27 +1,27 @@
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 # Path to your oh-my-zsh installation.
-export ZSH="/home/wanghao/.oh-my-zsh" 
+export ZSH="/home/zl/.oh-my-zsh"
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+ZSH_THEME="ys"
 plugins=(
   git autojump extract zsh-autosuggestions zsh-syntax-highlighting
 )
 source $ZSH/oh-my-zsh.sh
 
 export TERM=xterm-256color
-bindkey ',' autosuggest-accept
-alias jupwn="PWNLIB_NOTERM='1' jupyter lab"
-alias vmrun="/Applications/VMWare\ Fusion.app/Contents/Library/vmrun"
-alias manjarorun="vmrun start ~/Virtual\ Machines.localized/manjaro.vmwarevm nogui"
-alias manjarostop="vmrun stop ~/Virtual\ Machines.localized/manjaro.vmwarevm"
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-alias newt="tmux new -s"
-alias killt="tmux kill-session -t"
-alias help="tldr"
-alias lookip='curl cip.cc'
+# bindkey ',' autosuggest-accept
+# alias jupwn="PWNLIB_NOTERM='1' jupyter lab"
+# alias vmrun="/Applications/VMWare\ Fusion.app/Contents/Library/vmrun"
+# alias manjarorun="vmrun start ~/Virtual\ Machines.localized/manjaro.vmwarevm nogui"
+# alias manjarostop="vmrun stop ~/Virtual\ Machines.localized/manjaro.vmwarevm"
+# [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# alias newt="tmux new -s"
+# alias killt="tmux kill-session -t"
+# alias help="tldr"
+# alias lookip='curl cip.cc'
